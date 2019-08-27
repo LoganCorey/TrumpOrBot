fec97d1e7396da0e1596df2ccf0db51c4ca9cabc
Logan Corey <logan.corey@mail.utoronto.ca>
2019-08-26 18:59:07 -0700

    initial commit

---
+++ TweetScrapper.py
@@ -0,0 +1,15 @@
+
+class TweetScrapper:
+    def __init__(self, user):
+        """
+        """
+        self.user = user
+    
+    def getTweets(self, count):
+        print("hello")
+
+    def CreateTextFile(self):
+        pass
+
+if __name__ == "__main__":
+    pass
